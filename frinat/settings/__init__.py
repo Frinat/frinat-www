645d9cbef72591c82d64d8be4d27f6f63e20138d
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2014-02-25 10:25:33 +0100

    Ignore MPTT deprecation warnings and work around Django's default filter.

--- frinat/settings/__init__.py
+++ frinat/settings/__init__.py
@@ -1,1 +1,11 @@
+import warnings
+import logging
+import sys
+
+if not sys.warnoptions:
+    warnings.simplefilter('default', DeprecationWarning)
+    warnings.filterwarnings('ignore', '.*', DeprecationWarning, 'mptt.models', 305)
+    logging.captureWarnings(True)
+    sys.warnoptions.append('ignore::DeprecationWarning')
+
 from .common import *
